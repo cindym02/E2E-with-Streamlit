5b76313bb0419ebf0afb2b450be04ee1c06b2129
Cindy <whatsupcindy@gmail.com>
2021-12-19 23:40:57 -0500

    Created 507streamlit.py

---
+++ 507streamlit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 19 23:38:47 2021
+
+@author: cindy
+"""
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('HHA 507: Deployment of E2E Data Process with Streamlit')
