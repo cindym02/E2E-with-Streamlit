2e41e6a94d03d4ad545087034f26b1b904ffdcba
Cindy <whatsupcindy@gmail.com>
2021-12-20 00:23:50 -0500

    Updates

--- 507streamlit.py
+++ 507streamlit.py
@@ -11,11 +11,11 @@ import pandas as pd
 import numpy as np
 
 st.title('HHA 507: Deployment of E2E Data Process with Streamlit')
+st.write('Shuwen Tan :smiley:') 
 
 hospital_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital_2.csv')
 inpatient_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient_2.csv')
 outpatient_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
-
 
 @st.cache
 def load_hospitals():
@@ -32,4 +32,28 @@ def load_outpatient():
     outpatient_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
     return outpatient_df
 
+
+st.header('Hospital Data')
 st.dataframe(hospital_df)
+
+st.header('Inpatient Data Preview')
+st.dataframe(inpatient_df)
+
+st.header('Outpatient Data')
+st.dataframe(outpatient_df)
+
+st.header('Hospitals in NY Excluding Stony Brook')
+NY = hospital_df[hospital_df['state'] == 'NY']
+NY = hospital_df[hospital_df['state'] != 'NY']
+
+
+
+st.header('Question 1. How does Stony Brook compare to the rest of NY?')
+
+st.header('Question 2. What are the most expensive inpatient DRGs at Stony Brook?')
+
+st.header('Question 3. What are the most expensive outpatient DRGs at Stony Brook?')
+
+
+
+st.write('')
