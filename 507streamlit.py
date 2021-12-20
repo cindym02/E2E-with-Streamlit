6328c3b79282df660eacf993d77477c0417d1e0e
Cindy <whatsupcindy@gmail.com>
2021-12-20 00:03:38 -0500

    Update 507streamlit.py

--- 507streamlit.py
+++ 507streamlit.py
@@ -12,6 +12,10 @@ import numpy as np
 
 st.title('HHA 507: Deployment of E2E Data Process with Streamlit')
 
+hospital_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_hospital_2.csv')
+inpatient_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_inpatient_2.csv')
+outpatient_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
+
 
 @st.cache
 def load_hospitals():
@@ -27,3 +31,5 @@ def load_inpatient():
 def load_outpatient():
     outpatient_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_STATS_507/main/Week13_Summary/output/df_outpatient_2.csv')
     return outpatient_df
+
+st.dataframe(hospital_df)
