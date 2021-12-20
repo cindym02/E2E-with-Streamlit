f771b085f55471f00b78666d842e63a041ed792a
Cindy <whatsupcindy@gmail.com>
2021-12-20 01:02:44 -0500

    Update 507streamlit.py

--- 507streamlit.py
+++ 507streamlit.py
@@ -56,10 +56,12 @@ NY_notSB = hospital_df[hospital_df['state'] == 'NY']
 st.dataframe(NY_notSB)
 
 
-
 st.header('Question 1. How does Stony Brook compare to the rest of NY?')
 
+
 st.header('Question 2. What are the most expensive inpatient DRGs at Stony Brook?')
+SB_inpatientDRGs_pivot = SB_inpatient.pivot_table(index=['provider_id','drg_definition'],values=['average_total_payments'])
+SB_inpatientDRGs_order = SB_inpatientDRGs_pivot.sort_values(['average_total_payments'], ascending=False)
 
 st.header('Question 3. What are the most expensive outpatient DRGs at Stony Brook?')
 
